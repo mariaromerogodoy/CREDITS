c12f344d9ec6d5b5e4cf3d6f8559a6e9d42cc6d2
mariaromerogodoy <Romem899@newschool.edu>
2026-09-24 22:33:32 -0400

    POST

---
+++ Trial/java.js
@@ -0,0 +1,22 @@
+const credits = {
+  1: {
+    title: "Macintosh Computer, 1984",
+    credit: "Photograph by ___",
+    source: "Internet Archive",
+    link: "https://...",
+  },
+
+  2: {
+    title: "Early iPhone Interface",
+    credit: "Apple, 2007",
+    source: "Apple",
+    link: "https://...",
+  },
+
+  3: {
+    title: "Windows Interface",
+    credit: "Microsoft",
+    source: "Are.na",
+    link: "https://...",
+  },
+};
